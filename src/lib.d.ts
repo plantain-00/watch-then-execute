5e51e47f8780fd8e16de38a228f10a988686e650
York Yao <me@yorkyao.com>
2020-05-01 14:34:30 +0800

    chore: loose dependencies version

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -1,8 +1,3 @@
 declare module '*.json' {
   export const version: string
 }
-
-declare module 'lodash.debounce' {
-  function debounce(func: () => void, wait?: number): () => void
-  export = debounce
-}
